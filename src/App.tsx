25d635b6d39822b1a79df4d6db6b8699f80eff48
Lena Zhang <lena.zhang@tpisoftware.com>
2024-08-15 09:37:40 -0700

    feat: dashboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-import { Grid, Paper, styled, Button, TextField } from '@mui/material';
+import { Grid, Paper, styled, Button } from '@mui/material';
 import CustomDropDown from './component/UI/CustomDropDown';
 import TableauEmbed from './component/UI/TableauEmbed';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -38,6 +38,7 @@ const App: React.FC = () => {
     bathrooms: 0
   });
   const [url, setUrl] = useState<string | null>(null);
+  const today = dayjs();
 
   const handleDateChange = (dateType: 'startDate' | 'endDate', newValue: Dayjs | null) => {
     setFeatures((prevFeatures) => ({
@@ -47,8 +48,8 @@ const App: React.FC = () => {
   };
 
   const types = [
-    { value: "CONDO", label: 'condo' },
-    { value: "RESI", label: 'resi' }
+    { value: "CONDO", label: 'Condo' },
+    { value: "RESI", label: 'Residential' }
   ];
 
   const rooms = [];
@@ -57,14 +58,14 @@ const App: React.FC = () => {
   }
 
   const handleSearch = () => {
-    const api = `http://127.0.0.1:5000/xgboost`;
+    const api = `http://127.0.0.1:5000/predict`;
     // /xgboost
     // /random-forest-forecast
     fetch(api)
-      .then(response => response.blob()) // 将响应转换为 Blob 对象
+      .then(response => response.blob())
       .then(blob => {
-        const imageUrl = URL.createObjectURL(blob); // 创建一个对象URL用于显示图片
-        setUrl(imageUrl); // 假设 setMovies 用来存储图片URL
+        const imageUrl = URL.createObjectURL(blob);
+        setUrl(imageUrl);
       })
       .catch(error => {
         console.error('Error:', error);
@@ -89,18 +90,18 @@ const App: React.FC = () => {
       requestData.range = daysDifference;
     }
     console.log(requestData);
-    const api = `http://127.0.0.1:5000/xgboost`;
+    const api = `http://127.0.0.1:5000/predict`;
     fetch(api, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(requestData) // Ensure the key matches what your backend expects
+        body: JSON.stringify(requestData)
       })
-      .then(response => response.blob()) // 将响应转换为 Blob 对象
+      .then(response => response.blob())
       .then(blob => {
-        const imageUrl = URL.createObjectURL(blob); // 创建一个对象URL用于显示图片
-        setUrl(imageUrl); // 假设 setMovies 用来存储图片URL
+        const imageUrl = URL.createObjectURL(blob);
+        setUrl(imageUrl);
       })
       .catch(error => {
         console.error('Error:', error);
@@ -111,6 +112,9 @@ const App: React.FC = () => {
     <StyledContainer container spacing={3}>
       <Grid item xs={12}>
         <StyledPaper>
+          <div className="desc">
+            <h1>Price Predictor</h1>
+          </div>
           <Grid container spacing={2} alignItems="center">
             <Grid item xs={4} md={2}>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -119,6 +123,7 @@ const App: React.FC = () => {
                   value={features.startDate}
                   onChange={(newValue) => handleDateChange('startDate', newValue)}
                 />
+                {/* minDate={today} */}
               </LocalizationProvider>
             </Grid>
             <Grid item xs={4} md={2}>
@@ -127,6 +132,7 @@ const App: React.FC = () => {
                   label="End Date"
                   value={features.endDate}
                   onChange={(newValue) => handleDateChange('endDate', newValue)}
+                  minDate={features.startDate || today}
                 />
               </LocalizationProvider>
             </Grid>
@@ -156,7 +162,7 @@ const App: React.FC = () => {
             </Grid>
             <Grid item xs={4} md={2} style={{ textAlign: 'center' }}>
               <Button variant="contained" color="primary" style={{ height: '36px' }} onClick={handleSubmit}>
-                Click Me
+                Predict!
               </Button>
             </Grid>
           </Grid>
@@ -167,8 +173,89 @@ const App: React.FC = () => {
       </Grid>
       <Grid item xs={12} style={{ height: '600px', marginTop: '2%' }}>
         <StyledPaper style={{ height: '600px', overflow: 'auto' }}>
-          {/* Embed Tableau Dashboard here */}
-          <TableauEmbed />
+          <div className="desc">
+            <h1>Data Dashboard</h1>
+          </div>
+          <Grid container spacing={0} style={{ margin: '0px -100px 0px 0px' }}>
+            <Grid item xs={12} md={12}>
+              <iframe
+                src="/correlation_matrix_dashboard.html"
+                width="100%"
+                height="800px"
+                style={{ border: 'none' }}
+                title="Correlation Matrix Dashboard"
+              />
+            </Grid>
+          </Grid>
+          <Grid container spacing={0} style={{ margin: '-15`0px 0px' }}>
+            <Grid item xs={12} md={6}>
+              <iframe
+                src="/Avg_sales_price.html"
+                width="100%"
+                height="800px"
+                style={{ border: 'none' }}
+                title="Avg Sales Price"
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <iframe
+                src="/days_on_market.html"
+                width="100%"
+                height="800px"
+                style={{ border: 'none' }}
+                title="Correlation Matrix Dashboard"
+              />
+            </Grid>
+          </Grid>
+          <Grid container spacing={0}>
+            <Grid item xs={12} md={6}>
+              <iframe
+                src="/inventory_trends.html"
+                width="100%"
+                height="800px"
+                style={{ border: 'none' }}
+                title="Correlation Matrix Dashboard"
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <iframe
+                src="/market_supply_demand.html"
+                width="100%"
+                height="800px"
+                style={{ border: 'none' }}
+                title="Market Supply and Demand"
+              />
+            </Grid>
+          </Grid>
+          <Grid container spacing={0} style={{ margin: '-150px 0px' }}>
+            <Grid item xs={12} md={4}>
+              <iframe
+                src="/price_per_square_foot_distribution.html"
+                width="100%"
+                height="800px"
+                style={{ border: 'none' }}
+                title="Price Per Square Foot"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <iframe
+                src="/market_share.html"
+                width="100%"
+                height="800px"
+                style={{ border: 'none' }}
+                title="Market Share"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <iframe
+                src="/sales_distribution_by_price_range.html"
+                width="100%"
+                height="800px"
+                style={{ border: 'none' }}
+                title="Sales Distribution by Price Range"
+              />
+            </Grid>
+          </Grid>
         </StyledPaper>
       </Grid>
     </StyledContainer>
